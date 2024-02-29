8f40c6d54266bf2c9b5fbef4f43c767a32ec1662
Carlos Aguilera <ckarlos_95@hotmail.es>
2024-02-29 00:26:35 -0600

    Add background glitch effect

---
+++ src/shims.d.ts
@@ -0,0 +1,8 @@
+import type { AttributifyAttributes } from '@unocss/preset-attributify'
+
+declare global {
+  // Enable UnoCSS attributify mode in Astro
+  namespace astroHTML.JSX {
+    interface HTMLAttributes extends AttributifyAttributes { }
+  }
+}
