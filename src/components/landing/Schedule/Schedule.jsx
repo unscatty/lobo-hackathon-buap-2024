1345010e68ec00a7e1eba25f885c0cacec881c64
Juan Pablo de León <devleon00@gmail.com>
2024-03-20 00:19:36 -0600

    Add schedule data and new file

--- src/components/landing/Schedule/Schedule.jsx
+++ src/components/landing/Schedule/Schedule.jsx
@@ -56,10 +56,9 @@ const schedule = [
         id: 'session-3',
         time: '2024-04-18T13:30',
         timeEnd: '2024-04-18T14:15',
-        title: 'Master Class',
-        speaker: 'Alonso Berrelleza University Recruiter @ Microsoft',
-        description:
-          'University Recruiter en Microsoft, Alonso Berrelleza, nos hablará sobre las oportunidades de empleo y desarrollo profesional que ofrece Microsoft para estudiantes y recién egresados.',
+        title: 'Key Note Speaker',
+        speaker: 'TODO',
+        description: 'TODO',
       },
 
       {
@@ -115,7 +114,7 @@ const schedule = [
       {
         id: 'session-9',
         time: '2024-04-18T23:00',
-        timeEnd: '2024-04-18T01:00',
+        timeEnd: '2024-04-19T01:00',
         title: 'Torneo de Esports',
         speaker: 'Cualquiera',
         description:
@@ -131,72 +130,102 @@ const schedule = [
     sessions: [
       {
         id: 'session-1',
-        time: '9:00 AM',
-        title: 'Apertura',
-        speaker: '????',
+        time: '2024-04-18T23:00',
+        timeEnd: '2024-04-19T01:00',
+        title: 'Torneo de Esports',
+        speaker: 'Cualquiera',
+        description:
+          '¡Es hora de divertirse! Participa en el torneo de Esports que se llevará a cabo en el área de descanso. ¡No te lo pierdas!',
       },
 
       {
         id: 'session-2',
-        time: '10:00 AM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T07:00',
+        timeEnd: '2024-04-19T08:00',
+        title: 'Yoga',
+        speaker: 'Cualquiera',
+        description:
+          '¡Es hora de divertirse! Participa en el torneo de Esports que se llevará a cabo en el área de descanso. ¡No te lo pierdas!',
       },
 
       {
         id: 'session-3',
-        time: '11:00 AM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T08:00',
+        timeEnd: '2024-04-19T09:00',
+        title: 'Desayuno',
+        speaker: 'Cualquiera',
+        description:
+          '¡Es hora de recargar energías! Tendremos un receso para que puedas disfrutar de un delicioso desayuno y continuar con las actividades del día.',
       },
 
       {
         id: 'session-4',
-        time: '12:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T08:00',
+        timeEnd: '2024-04-19T09:00',
+        title: 'Mentoria',
+        speaker: 'Empresas',
+        description:
+          'Agenda tu mentoría con las empresas participantes. ¡No te pierdas esta oportunidad!',
       },
 
       {
         id: 'session-5',
-        time: '1:00 PM',
-        title: 'Comida',
-        speaker: '????',
+        time: '2024-04-19T12:00',
+        timeEnd: '2024-04-19T12:45',
+        title: 'Key Note Speaker',
+        speaker: 'Empresas',
+        description:
+          'Agenda tu mentoría con las empresas participantes. ¡No te pierdas esta oportunidad!',
       },
 
       {
-        id: 'session-6',
-        time: '2:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        id: 'session-5',
+        time: '2024-04-19T14:00',
+        timeEnd: '2024-04-19T14:45',
+        title: 'Comunidad GDG',
+        speaker: 'Representante de Comunidad GDG',
+        description:
+          'Conoce a la comunidad de Google Developers en tu universidad y descubre cómo puedes ser parte de ella. ¡No te pierdas esta oportunidad!',
       },
 
       {
         id: 'session-7',
-        time: '3:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T15:00',
+        timeEnd: '2024-04-19T16:00',
+        title: 'Comida + Ponencia Pitch (3:15 PM)',
+        speaker: 'BrujeriaTech',
+        description:
+          'Presentación de la ponencia de Pitch, donde se darán consejos para presentar tu proyecto de la mejor manera. ¡No te la pierdas!',
       },
 
       {
         id: 'session-8',
-        time: '4:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T14:05',
+        timeEnd: '2024-04-19T16:50',
+        title: 'Comunidad WTM',
+        speaker: 'Representante de Comunidad WTM',
+        description:
+          'Panel de discucion con miembros de una sola comunidad tech, con el objetivo de compartir porque es importante la comunidad, que beneficios tiene ser parte y como ser parte.',
       },
 
       {
         id: 'session-9',
-        time: '5:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-19T18:00',
+        timeEnd: '2024-04-19T19:30',
+        title: 'Mentoria',
+        speaker: 'Empresas',
+        description:
+          'Espacio reservado para que los equipos se reunan con los mentores de sus tracks, los cualesya conoceran el proyecto por la primer mentoria y tendran el primer entregable, con el objetivo de retroalimentar la idea. Duracion: 15min (por equipo) Tema: mentoria enfocada en presentar un buen pitch.',
       },
 
       {
         id: 'session-10',
-        time: '6:00 PM',
-        title: 'Cierre',
-        speaker: '????',
+        time: '2024-04-19T18:00',
+        timeEnd: '2024-04-19T19:30',
+        title: 'Cierre de instalaciones (8:00 PM)',
+        speaker: 'Participantes',
+        description:
+          'Es hora de cerrar las instalaciones y dar por terminado el primer día de actividades. ¡Gracias por tu participación!',
       },
     ],
   },
@@ -208,72 +237,69 @@ const schedule = [
     sessions: [
       {
         id: 'session-1',
-        time: '9:00 AM',
-        title: 'Apertura',
-        speaker: '????',
+        time: '2024-04-20T02:00',
+        timeEnd: '2024-04-20T04:00',
+        title: 'Torneo de Esports',
+        speaker: 'Cualquiera',
+        description:
+          '¡Es hora de divertirse! Participa en el torneo de Esports que se llevará a cabo en el área de descanso. ¡No te lo pierdas!',
       },
 
       {
         id: 'session-2',
-        time: '10:00 AM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-20T07:00',
+        timeEnd: '2024-04-20T08:00',
+        title: 'Entregable Final',
+        speaker: 'Hackathoneros',
+        description: `Entregables finales a subir a devPost:
+          Video con duracion de 3min, explicando su apliacion(demo).
+          Describir su aplicacion en un documento de de texto.
+          Repositorio de github.`,
       },
 
       {
         id: 'session-3',
-        time: '11:00 AM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-20T08:00',
+        timeEnd: '2024-04-20T09:00',
+        title: 'Deliberacion',
+        speaker: 'Jueces',
+        description: `Espacio donde lo jueces de cada reto tendran tiempo de revisar los entregables y finales y nombraran a los 3 finalistas.`,
       },
 
       {
         id: 'session-4',
-        time: '12:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-20T09:00',
+        timeEnd: '2024-04-20T09:15',
+        title: 'Anuncio de Finalistas',
+        speaker: 'Jueces',
+        description: `Se nombraran a 3 finalistas por cada track.`,
       },
 
       {
         id: 'session-5',
-        time: '1:00 PM',
-        title: 'Comida',
-        speaker: '????',
+        time: '2024-04-20T10:15',
+        timeEnd: '2024-04-20T11:45',
+        title: 'Presentacion Final',
+        speaker: 'Hackathoneros',
+        description: `Durante este tiempo los equipos finalistas de todos los tracks pasaran al main-stage y presentaran su aplicacion en un pitch de 5 minutos.`,
       },
 
       {
         id: 'session-6',
-        time: '2:00 PM',
-        title: 'Charla',
-        speaker: '????',
+        time: '2024-04-20T12:00',
+        timeEnd: '2024-04-20T13:00',
+        title: 'Presentacion Final',
+        speaker: 'Empresas',
+        description: `Se dara a conocer al equipo ganador de cada track.`,
       },
 
       {
         id: 'session-7',
-        time: '3:00 PM',
-        title: 'Charla',
-        speaker: '????',
-      },
-
-      {
-        id: 'session-8',
-        time: '4:00 PM',
-        title: 'Charla',
-        speaker: '????',
-      },
-
-      {
-        id: 'session-9',
-        time: '5:00 PM',
-        title: 'Charla',
-        speaker: '????',
-      },
-
-      {
-        id: 'session-10',
-        time: '6:00 PM',
-        title: 'Cierre',
-        speaker: '????',
+        time: '2024-04-20T13:00',
+        timeEnd: '2024-04-20T13:30',
+        title: 'Clausura',
+        speaker: 'Organización',
+        description: `Se dara a conocer al equipo ganador de cada track.`,
       },
     ],
   },
@@ -286,9 +312,11 @@ import 'react-vertical-timeline-component/style.min.css'
 import './schedule.css'
 
 const Schedule = () => {
+  const [activeDay, setActiveDay] = React.useState('day-1')
+
   return (
-    <section id="agenda" className={`${layout.section} relative z-1`}>
-      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+    <section id="agenda" className={`${layout.section} relative z-1 container mx-auto`}>
+      <div className="absolute z-[0] w-[60%] h-[60%] rounded-full blue__gradient bottom-40 right-[0%]" />
       <div className={layout.sectionInfo}>
         <h5
           className={`font-poppins text-dimWhite text-[24px] text-center w-full text-gradient font-bold mb-5`}
@@ -299,11 +327,32 @@ const Schedule = () => {
           Conoce Nuestra Agenda <br className="sm:block hidden" />
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 w-full mt-10 gap-10">
+        <div className="xl:hidden flex justify-center my-5 mx-auto">
+          <button
+            onClick={() => setActiveDay('day-1')}
+            className={`px-8 md:px-16 py-2 rounded border border-5e2176 ${activeDay === 'day-1' ? 'bg-white text-5e2176' : 'bg-5e2176 text-white'}`}
+          >
+            Day 1
+          </button>
+          <button
+            onClick={() => setActiveDay('day-2')}
+            className={`px-8 md:px-16 py-2 rounded border border-5e2176 ${activeDay === 'day-2' ? 'bg-white text-5e2176' : 'bg-5e2176 text-white'}`}
+          >
+            Day 2
+          </button>
+          <button
+            onClick={() => setActiveDay('day-3')}
+            className={`px-8 md:px-16 py-2 rounded border border-5e2176 ${activeDay === 'day-3' ? 'bg-white text-5e2176' : 'bg-5e2176 text-white'}`}
+          >
+            Day 3
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-3 w-full mt-10 gap-10">
           {schedule.map((event) => (
             <div
               key={event.id}
-              className="flex flex-col bg-black-gradient-2 p-6 rounded-[20px] relative"
+              className={` flex-col bg-black-gradient-2 p-6 rounded-[20px] relative ${activeDay === event.id ? 'flex' : 'hidden xl:flex'}`}
             >
               <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
                 {event.title}
